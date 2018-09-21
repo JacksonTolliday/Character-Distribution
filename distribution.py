a73370000481ae26fb4d746d6b4dbb7b3804e1bb
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-09-21 12:00:38 -0400

    Updated from Brython Server: 9/21/2018 12:00:37 PM

--- distribution.py
+++ distribution.py
@@ -1,7 +1,7 @@
 """
 distribution.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Jackson Tolliday
+Credit: https://stackoverflow.com/questions/6797984/how-to-convert-string-to-lowercase-in-python
 
 Assignment:
 
