4a29e04a2aeaa41e9c7da239be13e2e9dc1167f7
JacksonTolliday <42842756+JacksonTolliday@users.noreply.github.com>
2018-09-24 12:11:38 -0400

    Updated from Brython Server: 9/24/2018 12:11:37 PM

--- distribution.py
+++ distribution.py
@@ -41,17 +41,15 @@ import string
 inp = input(str('Please enter a string of text (the bigger the better): ')).lower()
 print('The distribution of characters in "{0}" is:'.format(inp))
 ltr = (sorted(list(inp)))
-ltr.append('0')
 con = ltr.count(' ')
 conp = ltr.count('.')
 conc = ltr.count(',')
 del ltr[0:(con+conp+conc)]
+print(ltr)
 for x in range(len(ltr)):
     if ltr[x] != ltr[x+1]:
         y = x+1
         print(y)
     else:
-        ltr == ltr
-
-print(ltr)
-print(y)
+        if ltr[-1] == ltr[x]:
+            break
